14b2509649bae0c789df1fdbd146db864d6e4fe5
Greg Bielleman <greg.bielleman@particular.net>
2017-02-07 14:34:59 +1100

    Bump Obsolete to 4.0

--- src/ServiceControl.Plugin.Nsb6.Heartbeat/obsoletes.cs
+++ src/ServiceControl.Plugin.Nsb6.Heartbeat/obsoletes.cs
@@ -1,6 +1,6 @@
 ﻿namespace ServiceControl.Plugin.Nsb6.Heartbeat
 {
-    [ObsoleteEx(ReplacementTypeOrMember = "ServiceControl.Features.Heartbeats", TreatAsErrorFromVersion = "2.0", RemoveInVersion = "3.0")]
+    [ObsoleteEx(ReplacementTypeOrMember = "ServiceControl.Features.Heartbeats", TreatAsErrorFromVersion = "2.0", RemoveInVersion = "4.0")]
 #pragma warning disable 1591
     public class Heartbeats
 #pragma warning restore 1591
